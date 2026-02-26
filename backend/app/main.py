d1b5eb6d1a5bcb4347c87b7533c133a5ad0952b2
BaidHarshita <harshita.baid@askin.xyz>
2026-02-27 00:49:52 +0530

    Established Database connection and checked the db status

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.database import engine
+from sqlalchemy import text
 
 app = FastAPI(
     title="AI Adaptive Learning System",
@@ -9,3 +11,9 @@ app = FastAPI(
 @app.get("/")
 def health_check():
     return {"status": "Backend is running"}
+
+@app.get("/test-db")
+def test_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        return {"database_status": "Connected", "result": result.scalar()}
