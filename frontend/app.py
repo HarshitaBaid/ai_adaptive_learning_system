f5b3f0755847104a00a22268b7ce1d12adf579e7
BaidHarshita <harshita.baid@askin.xyz>
2026-04-16 18:18:17 +0530

    add: request for backend wake up

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import threading
+import requests
+from config.settings import BASE_URL
+
+def wake_backend():
+    try:
+        requests.get(f"BASE_URL", timeout=5)
+    except:
+        pass
+
+threading.Thread(target=wake_backend, daemon=True).start()
 
 st.set_page_config(
     page_title="AI EdTech Platform",
